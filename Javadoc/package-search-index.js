488abc3fa4675921e0501f366b280dd5a908ce63
Doru Liviu <doru75c@yahoo.com>
2023-12-22 10:12:34 +0200

    Added Javadoc

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Annotations"},{"l":"BasePages"},{"l":"Enums"},{"l":"PageObjects"},{"l":"testSuite"},{"l":"Utils"}];updateSearchResults();
